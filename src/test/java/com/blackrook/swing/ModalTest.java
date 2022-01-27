59b3e2a6d69d08093f7c7cc028da7096a62b20a3
mtrop <mtropiano@gmail.com>
2022-01-27 02:51:35 -0500

    Modals

---
+++ src/test/java/com/blackrook/swing/ModalTest.java
@@ -0,0 +1,19 @@
+package com.blackrook.swing;
+
+import java.awt.event.KeyEvent;
+
+import static com.blackrook.swing.ContainerFactory.*;
+import static com.blackrook.swing.ComponentFactory.*;
+
+public final class ModalTest 
+{
+	public static void main(String[] args) 
+	{
+		Modal<Integer> m = modal("Test Modal", containerOf(node(label("This is a dialog."))),
+			choice("Cancel", KeyEvent.VK_C, 0),
+			choice("OK", KeyEvent.VK_O, 1)
+		);
+		System.out.println(m.open());
+		m.dispose();
+	}
+}
